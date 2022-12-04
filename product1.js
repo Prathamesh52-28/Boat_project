5463775b131fa14335f20fe427a6f225d1e2e28a
AkshataGanbote <akshataganbote61843@gmail.com>
2022-12-04 21:45:50 +0530

    Product files

---
+++ product1.js
@@ -0,0 +1,343 @@
+
+var productsList= [
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/m1_dac5a6b1-7937-4cdf-be70-76bb5a1fcf49_600x.png?v=1639153081",
+        name: "Rockerz 333 Pro",
+        rev: "4.9 | 154 reviews",
+        disc:"40% off",
+        price: "1799",
+        strikedoffprice: "2990",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/1_5_600x.png?v=1655534211",
+        name: "Rockerz 450 DC edition",
+        rev: "5.0 | 4 reviews",
+        disc:"62% off",
+        price: "1499",
+        strikedoffprice: "3990",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/2_3_f3ee5c27-4f14-4159-9fb2-dc60e7d6ec66_600x.png?v=1655536230",
+        name: "Rockerz 450",
+        rev: "4.9 | 8 reviews",
+        disc:"62% off",
+        price: "1499",
+        strikedoffprice: "3990",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/0cfa4417-0213-4b49-b78e-0ae68aeb7057_600x.png?v=1625046144",
+        name: "Rockerz 550",
+        rev: "4.8 | 354 reviews",
+        disc:"64% off",
+        price: "1799",
+        strikedoffprice: "4999",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/m2_600x.png?v=1639124121",
+        name: "Rockerz 330 Pro",
+        rev: "4.9 | 47 reviews",
+        disc:"40% off",
+        price: "1799",
+        strikedoffprice: "2990",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/rockerz-518-blue_600x.png?v=1613731627",
+        name: "Rockerz 518",
+        rev: "4.9 | 154 reviews",
+        disc:"67% off",
+        price: "1299",
+        strikedoffprice: "3990",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/3_6cd803a8-4aa4-48bd-95a7-b11d1f9fdeda_600x.png?v=1655888225",
+        name: "Rockerz 460",
+        rev: "4.9 | 27 reviews",
+        disc:"28% off",
+        price: "1799",
+        strikedoffprice: "2490",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/5d5377dc-d3dd-48ba-8ec9-2a1630146d8d_600x.png?v=1625045983",
+        name: "Rockerz 510",
+        rev: "4.8 | 142 reviews",
+        disc:"54% off",
+        price: "1599",
+        strikedoffprice: "3490",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/eb8e0fbd-c412-48b3-9c91-5b49ddf35800_17432771-6821-4dbf-86ac-9cb7c8dfe460_600x.png?v=1658829303",
+        name: "Rockerz 450R",
+        rev: "5.0 | 10 reviews",
+        disc:"67% off",
+        price: "1299",
+        strikedoffprice: "3990",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/e5881832-36f8-4c1c-a767-10f2c2a55a02_600x.png?v=1625046573",
+        name: "Rockerz 370",
+        rev: "4.8 | 89 reviews",
+        disc:"52% off",
+        price: "1199",
+        strikedoffprice: "2499",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/0cfa4417-0213-4b49-b78e-0ae68aeb7057_18fb3b69-5601-4680-a06c-b2a4c1a1343e_600x.png?v=1629538793",
+        name: "Rockerz 558",
+        rev: "5.0 | 2 reviews",
+        disc:"60% off",
+        price: "2999",
+        strikedoffprice: "4990",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main-img3_600x.png?v=1616562632",
+        name: "Rockerz 450 Pro",
+        rev: "4.7 | 67 reviews",
+        disc:"50% off",
+        price: "1999",
+        strikedoffprice: "3990",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/06bd8915-8db7-4575-9558-c97bf050d8d7_600x.png?v=1625044994",
+        name: "Rockerz 400",
+        rev: "4.8 | 95 reviews",
+        disc:"50% off",
+        price: "1499",
+        strikedoffprice: "2990",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/eb8e0fbd-c412-48b3-9c91-5b49ddf35800_600x.png?v=1625045744",
+        name: "Rockerz 450",
+        rev: "4.8 | 266 reviews",
+        disc:"67% off",
+        price: "1299",
+        strikedoffprice: "3990",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/1acf35c5-1ef2-4d34-859f-e085681e6d08_600x.png?v=1625046395",
+        name: "Rockerz 560",
+        rev: "4.8 | 18 reviews",
+        disc:"57% off",
+        price: "1699",
+        strikedoffprice: "3990",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main2_b66dce6b-710d-49cb-9d1c-2bc8c9c0ab15_600x.png?v=1645698328",
+        name: "Rockerz 425",
+        rev: "4.9 | 22 reviews",
+        disc:"53% off",
+        price: "1399",
+        strikedoffprice: "2990",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/48b829ed-bee6-492e-b9bc-1824d5c63bd4_1aa0a835-9bbd-432c-ad92-76d0c2b7a36d_600x.png?v=1646390147",
+        name: "TRebel Rockerz 550",
+        rev: "5.0 | 6 reviews",
+        disc:"60% off",
+        price: "1999",
+        strikedoffprice: "4999",
+        save: "Save ₹15 more on UPI payment",
+    },
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/fd0e55b0-ca9e-4aa8-b94f-5cca99189951_600x.png?v=1625045647",
+        name: "Rockerz 600",
+        rev: "4.8 | 34 reviews",
+        disc:"50% off",
+        price: "1999",
+        strikedoffprice: "3990",
+        save: "Save ₹15 more on UPI payment",
+    },
+];
+    
+//Function to append products//
+showList(productsList);
+
+function showList(productsList){
+    document.getElementById("parent").innerHTML = "";
+    productsList.map(function(elem, i){
+
+        var div = document.createElement("div");
+        div.setAttribute("class","card");
+
+        var img = document.createElement("img");
+        img.setAttribute("src",elem.img_link);
+
+        var imgBx = document.createElement("div");
+        imgBx.setAttribute("class","imgBx");
+        imgBx.append(img);
+
+        var name = document.createElement("h3");
+        name.textContent=elem.name;
+        name.setAttribute("class","name");
+
+        //
+        var stars = document.createElement("i");
+        stars.setAttribute("class", "fa-solid fa-star");
+
+        var rev = document.createElement("p");
+        rev.textContent=elem.rev;
+        rev.setAttribute("class","rev");
+
+        // Consists of star, rating, rev
+        reviewBox = document.createElement("div");
+        reviewBox.setAttribute("class","reviewBox");
+        reviewBox.append(stars, rev);
+        //
+
+        var x = document.createElement("hr");
+
+        var disc = document.createElement("p");
+        disc.textContent=elem.disc;
+        disc.setAttribute("class","disc");
+
+        var priceEle = document.createElement("p");
+        priceEle.setAttribute("class","price");
+        priceEle.textContent = "₹"+" "+elem.price;
+
+        var strikedoffprice = document.createElement("p");
+        strikedoffprice.setAttribute("class","strprice");
+        strikedoffprice.textContent = "₹"+" "+elem.strikedoffprice;
+
+        // Consists of Disc, Price & Striked Price
+        var priceparent = document.createElement("div");
+        priceparent.setAttribute("class","priceparent");
+        priceparent.append(disc,priceEle,strikedoffprice);
+
+        var save = document.createElement("p");
+        save.textContent=elem.save;
+        save.setAttribute("class","save");
+
+        var shopCartbtn = document.createElement("button");
+        shopCartbtn.textContent="ADD TO CART";
+        shopCartbtn.setAttribute("class","shopCartbtn");
+        shopCartbtn.addEventListener("click", function() {
+            addtocart(i);
+        });
+
+        // Constist of all contents in side of image
+        var description = document.createElement("div");
+        description.setAttribute("class","description");
+        description.append(name,reviewBox,x,priceparent,save,shopCartbtn); ///
+
+        div.append(imgBx,description);
+        document.getElementById("parent").append(div);
+    });
+}
+
+
+//Sorting 
+function sortFun(){
+    var selected=document.querySelector("#sort").value;
+    var sortProduct ;
+    if(selected=="Sort by Featured"){
+        console.log(productsList);
+        sortProduct =  productsList; 
+    }
+    else if(selected=="Price, low to high"){
+        sortProduct =  productsList.sort(function(a,b){
+        return parseFloat(a.price)-parseFloat(b.price);
+    })  
+    }
+    else if(selected=="Price, high to low"){
+        sortProduct =  productsList.sort(function(a,b){
+        return parseFloat(b.price)-parseFloat(a.price);
+    })  
+    }
+
+    else if(selected==="Alphabetically, A-Z"){
+        sortProduct =  productsList.sort(function(a,b){
+            if(a.name>b.name) return 1;
+            if(a.name<b.name) return -1;
+            return 0;
+        });  
+    }
+
+    else if(selected==="Alphabetically, Z-A"){
+        sortProduct =  productsList.sort(function(a,b){
+            if(a.name>b.name) return -1;
+            if(a.name<b.name) return 1;
+            return 0;
+    })  
+    }
+
+    console.log(sortProduct)
+
+    return showList(sortProduct);
+}
+
+
+// Add to Cart
+
+var cartProduct = JSON.parse(localStorage.getItem("cart")) || [];
+        
+function addtocart(index){
+    event.preventDefault()
+    alert("Product added to cart sucessfully !")
+    var flag = 0
+    var ind  = 0
+    var prod = productsList.filter(function(elem,i){
+        return i == index
+    })
+    var cartp = cartProduct.filter(function(elem,i){
+        if(elem.name === prod[0].name){
+            flag = 1
+            ind = i
+        }
+    })
+    if(flag == 0){
+        prod[0].qty = 1
+        cartProduct.push(prod[0])
+    }
+    else{
+        cartProduct[ind].qty++
+        flag = 0
+    }
+    localStorage.setItem("cart",JSON.stringify(cartProduct))
+    console.log(cartProduct)
+    var cartItems = JSON.parse(localStorage.getItem("cart")) || []
+        var items =  cartItems.length
+        console.log(items)
+        if(items>0){
+        document.querySelector(".nitems").textContent = items
+        }
+    
+}
+      
+var cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+var items =  cartItems.length;
+console.log(items);
+if(items>0){
+    document.querySelector(".nitems").textContent = items
+}
+
+// Mouse Moves
+
+function displayData(){
+    var keep = document.querySelector(".curser")
+    keep.style.display = "grid";
+    keep.style.background = "white";
+    document.querySelector("body").addEventListener("mouseleave",removeData)
+  }
+  document.querySelector(".mouse").addEventListener("mouseover",displayData)
+  
+function removeData(){
+    var keep = document.querySelector(".curser")
+    keep.style.display = "none";
+}
+document.querySelector("body").addEventListener("mouseleave",removeData)
+   
---
+++ product2.js
@@ -0,0 +1,382 @@
+
+var productsList= [
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/1-2_300x.png?v=1653637007",
+        name: "Wave Neo",
+        rev: "4.7 | 19 reviews",
+        discount:"70%off",
+        price: "1799",
+        strikedoffprice: "5990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/blue_d8db0011-02f7-43d6-9fe7-7a71a9ed86d2_300x.png?v=1654312290",
+        name: "Wave Connect",
+        rev: "4.9 | 88 reviews",
+        discount:"57% off",
+        price: "2999",
+        strikedoffprice: "6990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/blue_58bd5e28-de95-4f31-909d-6bce484bdbd9_300x.png?v=1658294451",
+        name: "Wave Select",
+        rev: "5.0 | 13 reviews",
+        discount:"64% off",
+        price: "2499",
+        strikedoffprice: "6990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/Blaze-FI-Red04_300x.png?v=1658123186",
+        name: "Blaze",
+        rev: "5.0 | 20 reviews",
+        discount:"47% off",
+        price: "3699",
+        strikedoffprice: "6990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main-images-2_300x.png?v=1655198531",
+        name: "Xtend Sport",
+        rev: "4.9 | 7 reviews",
+        discount:"64% off",
+        price: "2499",
+        strikedoffprice: "6990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/a6549acb-b075-4c3e-8ed3-9c9fcba32d42_300x.png?v=1625046216",
+        name: "Storm",
+        rev: "4.9 | 1010 reviews",
+        discount:"59% off",
+        price: "2449",
+        strikedoffprice: "5990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/5752f5e4-89e0-4f84-b7fe-4d9f85771046_300x.png?v=1625157753",
+        name: "Watch Flash",
+        rev: "4.8 | 48 reviews",
+        discount:"36% off",
+        price: "4499",
+        strikedoffprice: "6990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/xtend_black_300x.png?v=1650387008",
+        name: "Xtend",
+        rev: "4.8 | 146 reviews",
+        discount:"67% off",
+        price: "2659",
+        strikedoffprice: "7990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main3.3_9198fbdb-47fc-4ba1-878f-10d3acdb3d9c_300x.png?v=1646394746",
+        name: "TRebel Blaze",
+        rev: "5.0 | 6 reviews",
+        discount:"43% off",
+        price: "3999",
+        strikedoffprice: "6990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/mainblack_300x.png?v=1658994702",
+        name: "Iris",
+        rev: "5.0 | 2 reviews",
+        discount:"63% off",
+        price: "4499",
+        strikedoffprice: "11999",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main1_f30681d9-fddb-4997-bfcb-503fcc258a5c_300x.png?v=1646392933",
+        name: "Matrix",
+        rev: "5.0 | 11 reviews",
+        discount:"58% off ",
+        price: "4999",
+        strikedoffprice: "11990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main6_2_8e1cdd84-4a34-4e92-97c5-0e1d4ba6da46_300x.png?v=1646393710",
+        name: "TRebel Xtend",
+        rev: "5.0 | 3 reviews",
+        discount:"56% off",
+        price: "3499",
+        strikedoffprice: "7990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main_2b98dda1-57d3-40ee-80f0-4687b8f8a72b_300x.png?v=1662537789",
+        name: "Watch Xtend DC Edition",
+        rev: "Be the first to review",
+        discount:"12% off",
+        price: "3499",
+        strikedoffprice: "3990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/storm-black_82c0d3c8-da5c-49f1-8d13-08055d7afac8_300x.png?v=1623843234",
+        name: "Storm RTL",
+        rev: "5.0 | 3 reviews",
+        discount:"50% off",
+        price: "2999",
+        strikedoffprice: "5990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/Matrix-FI_Blue02_da7a821f-c621-4af6-aec2-7d647df3a5db_300x.png?v=1658994355",
+        name: "TRebel Matrix",
+        rev: "5.0 | 1 review",
+        discount:"58% off",
+        price: "4999",
+        strikedoffprice: "11990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/2_1e5f7cde-1976-4877-b92b-a685bed27233_300x.png?v=1662123878",
+        name: "Storm Pro Call",
+        rev: "5.0 | 10 reviews",
+        discount:"64% off",
+        price: "3499",
+        strikedoffprice: "9799",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/20dbeecf-4dc2-48c6-8dae-8a31f1746f44_300x.png?v=1625046513",
+        name: "Xplorer O2",
+        rev: "4.8 | 29 reviews",
+        discount:"33% off",
+        price: "3999",
+        strikedoffprice: "5990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/xtend_black_ae26d6cf-4552-45a6-8a61-42160e1b4407_300x.png?v=1658829594",
+        name: "Watch Xtend RTL",
+        rev: "Be the fist to review",
+        discount:"56% off",
+        price: "3499",
+        strikedoffprice: "7990",
+        save: "Save ₹15 more on UPI payment",
+    },
+
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/5752f5e4-89e0-4f84-b7fe-4d9f85771046_44573016-8eae-4ed9-8fa7-57f96575ba8f_300x.png?v=1658829787",
+        name: "Watch Flash RTL",
+        rev: "Be the first to review",
+        discount:"57% off",
+        price: "2999",
+        strikedoffprice: "6990",
+        save: "Save ₹15 more on UPI payment",
+    }, 
+    
+    {
+        img_link: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/Green_4_300x.png?v=1651215796",
+        name: "Mystiq",
+        rev: "4.8 | 5 reviews",
+        discount:"64% off",
+        price: "2499",
+        strikedoffprice: "6990",
+        save: "Save ₹15 more on UPI payment",
+    },
+    
+];
+    
+//Function to append products//
+showList(productsList);
+
+function showList(productsList){
+    document.getElementById("parent").innerHTML = "";
+    productsList.map(function(elem, i){
+
+        var div = document.createElement("div");
+        div.setAttribute("class","card");
+
+        var img = document.createElement("img");
+        img.setAttribute("src",elem.img_link);
+
+        var imgBx = document.createElement("div");
+        imgBx.setAttribute("class","imgBx");
+        imgBx.append(img);
+
+        var name = document.createElement("h3");
+        name.textContent=elem.name;
+        name.setAttribute("class","name");
+
+        //
+        var stars = document.createElement("i");
+        stars.setAttribute("class", "fa-solid fa-star");
+
+        var rev = document.createElement("p");
+        rev.textContent=elem.rev;
+        rev.setAttribute("class","rev");
+
+        // Consists of star, rating, rev
+        reviewBox = document.createElement("div");
+        reviewBox.setAttribute("class","reviewBox");
+        reviewBox.append(stars, rev);
+        //
+
+        var x = document.createElement("hr");
+
+        var disc = document.createElement("p");
+        disc.textContent=elem.disc;
+        disc.setAttribute("class","disc");
+
+        var priceEle = document.createElement("p");
+        priceEle.setAttribute("class","price");
+        priceEle.textContent = "₹"+" "+elem.price;
+
+        var strikedoffprice = document.createElement("p");
+        strikedoffprice.setAttribute("class","strprice");
+        strikedoffprice.textContent = "₹"+" "+elem.strikedoffprice;
+
+        // Consists of Disc, Price & Striked Price
+        var priceparent = document.createElement("div");
+        priceparent.setAttribute("class","priceparent");
+        priceparent.append(disc,priceEle,strikedoffprice);
+
+        var save = document.createElement("p");
+        save.textContent=elem.save;
+        save.setAttribute("class","save");
+
+        var shopCartbtn = document.createElement("button");
+        shopCartbtn.textContent="ADD TO CART";
+        shopCartbtn.setAttribute("class","shopCartbtn");
+        shopCartbtn.addEventListener("click", function() {
+            addtocart(i);
+        });
+
+        // Constist of all contents in side of image
+        var description = document.createElement("div");
+        description.setAttribute("class","description");
+        description.append(name,reviewBox,x,priceparent,save,shopCartbtn); ///
+
+        div.append(imgBx,description);
+        document.getElementById("parent").append(div);
+    });
+}
+
+
+//Sorting 
+function sortFun(){
+    var selected=document.querySelector("#sort").value;
+    var sortProduct ;
+    if(selected=="Sort by Featured"){
+        console.log(productsList);
+        sortProduct =  productsList; 
+    }
+    else if(selected=="Price, low to high"){
+        sortProduct =  productsList.sort(function(a,b){
+        return parseFloat(a.price)-parseFloat(b.price);
+    })  
+    }
+    else if(selected=="Price, high to low"){
+        sortProduct =  productsList.sort(function(a,b){
+        return parseFloat(b.price)-parseFloat(a.price);
+    })  
+    }
+
+    else if(selected==="Alphabetically, A-Z"){
+        sortProduct =  productsList.sort(function(a,b){
+            if(a.name>b.name) return 1;
+            if(a.name<b.name) return -1;
+            return 0;
+        });  
+    }
+
+    else if(selected==="Alphabetically, Z-A"){
+        sortProduct =  productsList.sort(function(a,b){
+            if(a.name>b.name) return -1;
+            if(a.name<b.name) return 1;
+            return 0;
+    })  
+    }
+
+    console.log(sortProduct)
+
+    return showList(sortProduct);
+}
+
+
+// Add to Cart
+
+var cartProduct = JSON.parse(localStorage.getItem("cart")) || [];
+        
+function addtocart(index){
+    event.preventDefault()
+    alert("Product added to cart sucessfully !")
+    var flag = 0
+    var ind  = 0
+    var prod = productsList.filter(function(elem,i){
+        return i == index
+    })
+    var cartp = cartProduct.filter(function(elem,i){
+        if(elem.name === prod[0].name){
+            flag = 1
+            ind = i
+        }
+    })
+    if(flag == 0){
+        prod[0].qty = 1
+        cartProduct.push(prod[0])
+    }
+    else{
+        cartProduct[ind].qty++
+        flag = 0
+    }
+    localStorage.setItem("cart",JSON.stringify(cartProduct))
+    console.log(cartProduct)
+    var cartItems = JSON.parse(localStorage.getItem("cart")) || []
+        var items =  cartItems.length
+        console.log(items)
+        if(items>0){
+        document.querySelector(".nitems").textContent = items
+        }
+    
+}
+      
+var cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+var items =  cartItems.length;
+console.log(items);
+if(items>0){
+    document.querySelector(".nitems").textContent = items
+}
+
+// Mouse Moves
+
+function displayData(){
+    var keep = document.querySelector(".curser")
+    keep.style.display = "grid";
+    keep.style.background = "white";
+    document.querySelector("body").addEventListener("mouseleave",removeData)
+  }
+  document.querySelector(".mouse").addEventListener("mouseover",displayData)
+  
+function removeData(){
+    var keep = document.querySelector(".curser")
+    keep.style.display = "none";
+}
+
+document.querySelector("body").addEventListener("mouseleave",removeData)
+   
